65a66fd56498f44f40427360ade991baa564c72c
Md. Shariful Islam <imdshariful171@gmail.com>
2020-10-26 23:10:49 +0600

    value pass via parameter

---
+++ ASP.NET MVC Lab Task/ASP.NET MVC Lab Task/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Lab_Task.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Home
+        public String Test()
+        {
+            return "Hello C#";
+            //return View();
+        }
+        public String anotherTest(int? id =null)
+        {
+            return "This is another method " + (id.ToString()+2);
+            //return View();
+        }
+    }
+}
