d995330d66f406c44d495c32a9362b3a23b24a5f
Md. Shariful Islam <imdshariful171@gmail.com>
2020-10-31 01:17:31 +0600

    data fetch with post req.

--- ASP.NET MVC Lab Task/ASP.NET MVC Lab Task/Controllers/HomeController.cs
+++ ASP.NET MVC Lab Task/ASP.NET MVC Lab Task/Controllers/HomeController.cs
@@ -6,17 +6,37 @@ using System.Web.Mvc;
 
 namespace ASP.NET_MVC_Lab_Task.Controllers
 {
-    public class TestController : Controller
+    public class HomeController : Controller
     {
         // GET: Home
-        public String Test()
+        public ActionResult Index   ()
         {
-            return "Hello C#";
-            //return View();
+            //return RedirectToAction("AnotherTest");
+            //return "Hello C#";
+            if(Request.HttpMethod == "GET")
+            {
+                return View("Test");
+            }
+            else if(Request.HttpMethod == "POST")
+            {
+                string name = Request["name"];
+                string userName = Request["userName"];
+                string password = Request["password"];
+                string confirmPassword = Request["confirmPassword"];
+                string bloodGroup = Request["bloodGroup"];
+                string gender = Request["gender"];
+                string dateOfBirth= Request["dateOfBirth"];
+
+                return Content("<h1>Name is: " + name + "</h1>");
+            }
+            else
+            {
+                return Content("<h1>It doesn't work</h1>");
+            }
         }
         public String anotherTest(int? id =null)
         {
-            return "This is another method " + (id.ToString()+2);
+            return "This is another method " + id.ToString();
             //return View();
         }
     }
